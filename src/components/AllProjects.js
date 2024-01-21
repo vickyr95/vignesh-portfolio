12cb9771f03a9d005f9bb5bb85b9e88cb1f20999
ShobiyaL <sofy7468612@gmail.com>
2024-01-21 16:10:15 +0530

    image-project

--- src/components/AllProjects.js
+++ src/components/AllProjects.js
@@ -6,13 +6,13 @@ import { motion } from 'framer-motion';
 const AllProjects = () => {
   return (
     <motion.div
-      className='min-h-screen py-10 mx-auto dark:bg-black dark:text-gray-300'
+      className='min-h-screen py-10  md:mx-auto dark:bg-black dark:text-gray-300'
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 1, ease: 'easeOut' }}
       //   exit={{ opacity: 1 }}
     >
-      <div className='container mx-auto '>
+      <div className='container  '>
         <div className=' flex  gap-4 items-center justify-between mx-10   '>
           <span className='  py-5'>
             <Link to='/' className='border rounded-md px-5 py-2 '>
@@ -25,7 +25,7 @@ const AllProjects = () => {
           <div></div>
         </div>
 
-        <div className='container   md:mx-10 px-10  my-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+        <div className='container   md:mx-10  px-10  my-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
           {projectsData.map((project, index) => (
             <div key={index} className=' '>
               <ProjectCard {...project} />
